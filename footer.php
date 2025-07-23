f9030b8653da5ebe00d4bb544cf2d036c9e35eea
PythonLinks <34622952+PythonLinks@users.noreply.github.com>
2025-07-23 07:20:51 +0200

    Deleted CA GP stuff from footer. Now it is just one line of links to wordpress, green party theme, and our discord server.

--- footer.php
+++ footer.php
@@ -7,21 +7,10 @@
 ?>
        </div><!--.site-content-->
 <footer>
-<div  style = "border-top: 3px solid #ddd;"> 
-<img alt="California Green Party" src="<?php echo get_template_directory_uri(); ?>/cagreens/logo-gpca-us-1.png" /style="width: 119px; height: 119px; margin:auto; display:block;" />
-</div>
-    <div style = "font-size:24px;color:#888888">
-
-<p class= "aligncenter" style="font-size:24px">
-
-<br />We welcome all Californians who share our Green vision to join us.</p>
-<p class = "aligncenter" style="font-size:18px">GREEN PARTY OF CALIFORNIA<br />PO Box 485<br />San Francisco, CA 94104<br />(916) 448-3437</p>
-</div>
-
       <div class = "aligncenter" style = "border-top: 3px solid #ddd;">
       Powered by <a href="https://wordpress.org" rel="nofollow">WordPress</a> | Theme: <a href="https://github.com/PythonLinks/green-party" rel="nofollow">Green Party Theme</a> | Developed By: <a href="https://discord.gg/keMrNVCu7F" rel="nofollow">Green Party Tech</a> 
       </div>
-            </footer>
+</footer>
 
         </div><!--.page-container-->
 
